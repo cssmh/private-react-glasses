e94c38c459ae3e58f182abe94241087124b9fa37
momen210 <touristmomen@gmail.com>
2024-01-26 19:48:33 +0600

    setup completed, time for auth

--- src/Pages/Login/Login.jsx
+++ src/Pages/Login/Login.jsx
@@ -1,45 +1,72 @@
-import { Link } from 'react-router-dom';
-import SocialLogin from './SocialLogin';
+import { Link } from "react-router-dom";
+import SocialLogin from "./SocialLogin";
 
 const Login = () => {
-    return (
-        <>
-            <div className="hero min-h-screen bg-base-200">
-                <div className="hero-content flex-col lg:flex-row-reverse">
-                    <div className="text-center lg:text-left">
-                        <h1 className="text-5xl font-bold">Login now!</h1>
-                        <p className="py-6">Provident cupiditate voluptatem et in. Quaerat fugiat ut assumenda excepturi exercitationem quasi. In deleniti eaque aut repudiandae et a id nisi.</p>
-                    </div>
-                    <div className="card flex-shrink-0 w-full max-w-sm shadow-2xl bg-base-100">
-                        <div className="card-body">
-                            <div className="form-control">
-                                <label className="label">
-                                    <span className="label-text">Email</span>
-                                </label>
-                                <input type="text" placeholder="email" className="input input-bordered" />
-                            </div>
-                            <div className="form-control">
-                                <label className="label">
-                                    <span className="label-text">Password</span>
-                                </label>
-                                <input type="text" placeholder="password" className="input input-bordered" />
-                                <label className="label">
-                                    <a href="#" className="label-text-alt link link-hover">Forgot password?</a>
-                                </label>
-                            </div>
-                            <div className="form-control mt-6 p-0">
-                                <button className="btn btn-neutral">Login</button>
-                            </div>
-                            <label className="label">
-                                New here? <Link to="/register" className="label-text-alt link link-hover">Create an account</Link>
-                            </label>
-                            <SocialLogin />
-                        </div>
-                    </div>
+  return (
+    <>
+      <div className="hero bg-base-200">
+        <div className="hero-content flex-col lg:flex-row-reverse">
+          <div className="text-center lg:text-left">
+            <h1 className="text-5xl font-bold my-2">Login now!</h1>
+            <p>
+              Lorem, ipsum dolor sit amet consectetur adipisicing elit. Laborum,
+              doloribus.
+            </p>
+          </div>
+          <div className="card flex-shrink-0 w-full max-w-sm shadow-2xl bg-base-100">
+            <form>
+              <div className="card-body">
+                <div className="form-control">
+                  <label className="label">
+                    <span className="label-text">Email</span>
+                  </label>
+                  <input
+                    type="email"
+                    name="email"
+                    placeholder="Email"
+                    className="input input-bordered"
+                    required
+                  />
                 </div>
-            </div>
-        </>
-    );
+                <div className="form-control">
+                  <label className="label">
+                    <span className="label-text">Password</span>
+                  </label>
+                  <input
+                    type="password"
+                    name="password"
+                    placeholder="Password"
+                    className="input input-bordered"
+                    required
+                  />
+                  <label className="label">
+                    <a href="#" className="label-text-alt link link-hover">
+                      Forgot password?
+                    </a>
+                  </label>
+                </div>
+                <div className="form-control mt-6 p-0">
+                  <button type="submit" className="btn btn-neutral">
+                    Login
+                  </button>
+                </div>
+                <label className="label">
+                  New here?{" "}
+                  <Link
+                    to="/register"
+                    className="label-text-alt link link-hover text-sm text-green-400"
+                  >
+                    Create an account
+                  </Link>
+                </label>
+                <SocialLogin />
+              </div>
+            </form>
+          </div>
+        </div>
+      </div>
+    </>
+  );
 };
 
 export default Login;
--- src/Root.jsx
+++ src/Root.jsx
@@ -1,19 +1,17 @@
-
-import { Outlet } from 'react-router-dom'
-import './App.css'
-import NavBar from './Components/NavBar/NavBar'
-import Footer from './Components/Footer/Footer'
+import { Outlet } from "react-router-dom";
+import NavBar from "./Components/NavBar/NavBar";
+import Footer from "./Components/Footer/Footer";
 
 function Root() {
-
   return (
-    <div className='2xl:container mx-auto'>
+    <div className="2xl:container mx-auto">
       <NavBar />
-      <Outlet />
+      <div className="min-h-[80vh]">
+        <Outlet />
+      </div>
       <Footer />
     </div>
-  )
+  );
 }
 
-
-export default Root
+export default Root;
